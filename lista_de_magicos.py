6807e59c0db7d15c911e1b16f1f04798a7b0dffc
James Costa <130270571+JAMESCOSTA01@users.noreply.github.com>
2023-10-12 12:07:32 -0300

    Add files via upload

---
+++ lista_de_magicos.py
@@ -0,0 +1,6 @@
+magicians = ["alice","david", "carolina"]
+for magician in magicians:
+    print(magician.title() + ", esse foi um ótimo truque!")
+    print("Mal posso esperar para ver o seu próximo truque, " +magician.title() + ".\n")
+    print("Que legal vocês todos, foi um grande show de mágica!")
+    
